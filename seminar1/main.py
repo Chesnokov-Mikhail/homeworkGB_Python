f7fc075ce0e07e3d0399639dbbb2f76a64f96204
Chesnokov M.A <chesnokov_ma@mail.ru>
2022-07-11 02:05:21 +0500

    task 1 seminar1

--- seminar1/main.py
+++ seminar1/main.py
@@ -1,8 +1,22 @@
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# 1. Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+#    Пример:
+# - 6 -> да
+# - 7 -> да
+# - 1 -> нет
+
+def is_weekday(day):
+    if weekday==6 or weekday==7:
+        return True
+    else:
+        return False
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    in_day = input('Введите цифру, обозначающую день недели [1 - 7]: ')
+    try:
+        weekday = int(in_day)
+    except:
+        print('Необходимо ввести число')
+    if (1 <= weekday <= 7):
+        print(f'{weekday} -> Да') if is_weekday(weekday) else print(f'{weekday} -> Нет')
+    else:
+        print('Введите число из диапазона [1 - 7]')
